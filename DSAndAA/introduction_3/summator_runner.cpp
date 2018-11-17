1a1775b8ea79c97f63f5e081713112f9a3db2019
Monkey <1466947023@qq.com>
2018-11-17 16:08:05 +0800

    或许是喝醉了。忘了提交某些程序

--- DSAndAA/introduction_3/summator_runner.cpp
+++ DSAndAA/introduction_3/summator_runner.cpp
@@ -3,3 +3,20 @@
  * Copyright (c) 2018/11/12 Xiaozhong. All rights reserved.
  */
 
+#include <string>
+#include <iostream>
+#include "BigInteger.h"
+
+using namespace std;
+
+int main() {
+    string data1, data2;
+    cin >> data1 >> data2;
+    BigInteger bigInteger1(data1);
+    BigInteger bigInteger2(data2);
+    char * result = bigInteger1.add(bigInteger2);
+    for (int i = 0; i < bigInteger1.get_length()+1; ++i) {
+        cout << result[i];
+    }
+}
+
